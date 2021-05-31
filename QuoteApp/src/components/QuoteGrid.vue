3a44386febefe86c88ab93a4303e602fbe328208
Sunny Singh <sunnyon33@gmail.com>
2021-05-31 20:55:19 +0530

    Add files via upload

---
+++ QuoteApp/src/components/QuoteGrid.vue
@@ -0,0 +1,30 @@
+<template>
+         
+<div class="row">
+
+    <app-quote v-for="(quote,index) in quotes" :key="quote" :index="index" @click.native="deleteQuote">{{quote}}</app-quote>
+
+
+</div>
+    
+</template>
+<script>
+import Quote from './Quote.vue'
+export default {
+   props:['quotes'],
+   components:{
+   appQuote:Quote
+   },
+   methods:{
+       deleteQuote(index){
+           this.$emit('quoteDeleted',index);
+
+       }
+   }
+}
+</script>
+
+<style scoped>
+
+
+</style>
